853199ef912a876cf1872002b0938a90202d075b
Gabby <ortmangm@iastate.edu>
2015-11-23 17:19:27 -0600

    add new call

--- Swampyw/swampyw.py
+++ Swampyw/swampyw.py
@@ -22,3 +22,12 @@ class Shrek(object):
             return self.json_file['Shrek']['Character Lines'][character.upper()]
         else: 
             raise ValueError("That character is not in the cinematic classic, Shrek!")
+
+    def get_nth_line(self, character, line): 
+        if character.upper() in self.json_file['Shrek']['All Characters'] and len(self.json_file['Shrek']['Character Lines'][character.upper()]) > line: 
+            return self.json_file['Shrek']['Character Lines'][character.upper()][line]
+        else: 
+            raise ValueError("This is bad error handling. Either the character you've picked isn't in the cinematic classic, Shrek or they don't have that many lines")
+
+shrek = Shrek() 
+print shrek.get_nth_line("Fiona", 10)
